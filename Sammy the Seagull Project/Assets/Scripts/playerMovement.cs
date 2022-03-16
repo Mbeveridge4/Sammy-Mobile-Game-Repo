b4f6786ec5f26339df992ea35442d57af03fbeaf
Mbeveridge4 <L000225343@fife.ac.uk>
2022-03-16 14:48:06 +0000

    Added player control with velocity and speed to the asset.
    Edited playerMovement Script to allow us to control the karenSprite.

--- Sammy the Seagull Project/Assets/Scripts/playerMovement.cs
+++ Sammy the Seagull Project/Assets/Scripts/playerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))] //Declares that the script cant run on an object without a rigidbody2D and gives it one if it doesnt have it
 public class playerMovement : MonoBehaviour
 {
+    public float moveSpeed = 3.0f; 
 
     private Rigidbody2D physicsBody = null; 
 
@@ -14,12 +15,18 @@ public class playerMovement : MonoBehaviour
     {
         physicsBody = GetComponent<Rigidbody2D>(); //calls the rigidbody
 
-        physicsBody.velocity = new Vector2(2,0); //sets the velocity variable of the physicsBody (x,y) per second
+       // physicsBody.velocity = new Vector2(2,0); //sets the velocity variable of the physicsBody (x,y) per second
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Ask Unity's Input manager for the current value of the horizontal Axis.
+        // This will be between -1 and 1
+       float axisVal = Input.GetAxis("Horizontal");
+
+        Vector2 newVel = new Vector2(axisVal, 0);
+        newVel = newVel * moveSpeed;
+        physicsBody.velocity = newVel;
     }
 }
